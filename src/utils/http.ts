dbfaf98bc2774da2fd7769c217b3333be4b21d6e
Nguyễn Mạnh Hồng <hongnmp@gmail.com>
2024-07-27 12:13:48 +0700

    fusp

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,14 +1,29 @@
 import axios, { AxiosError, type AxiosInstance } from 'axios'
+import { getAccessTokenFromLS } from './auth'
 
 export class Http {
   instance: AxiosInstance
+  private accessToken: string
   constructor() {
+    this.accessToken = getAccessTokenFromLS()
     this.instance = axios.create({
-      baseURL: 'http://localhost:8080/api/v1/',
+      baseURL: 'http://0.tcp.ap.ngrok.io:14755/api/v1/',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       }
     })
+    this.instance.interceptors.request.use(
+      (config) => {
+        if (this.accessToken && config.headers) {
+          config.headers.authorization = 'Bearer ' + this.accessToken
+          return config
+        }
+        return config
+      },
+      (error) => {
+        return Promise.reject(error)
+      }
+    )
   }
 }
 const http = new Http().instance
